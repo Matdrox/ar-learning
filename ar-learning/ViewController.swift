b8b720c2c4095315efca43d55dec5ce7d597604c
Matei Cananau <mateicananau@gmail.com>
2023-04-16 12:16:18 +0200

    Initial Commit

---
+++ ar-learning/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  ar-learning
+//
+//  Created by Matei Cananau on 2023-04-16.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
